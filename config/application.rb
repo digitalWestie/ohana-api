8501aa6118d529c9fb299dce7d80a45b4f4ed65b
Rory Gianni <giannichan@gmail.com>
2014-10-02 00:44:20 +0100

    set en-gb as default locale and add localisations

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,10 @@ module OhanaApi
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :"en-GB"
+    config.i18n.available_locales = ['en-GB', :en]
+    config.i18n.default_locale = 'en-GB'
+    config.i18n.fallbacks = true
+    config.i18n.fallbacks = [:en]
 
     config.active_record.schema_format = :sql
 
