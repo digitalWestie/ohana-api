42b4b35c3e07b2c90262bd30811e7f28a7ee8e61
Rory Gianni <giannichan@gmail.com>
2014-10-27 12:08:28 +0000

    provides default values for new columns where null is not accepted

--- db/migrate/20140929221750_add_fields_to_organization.rb
+++ db/migrate/20140929221750_add_fields_to_organization.rb
@@ -2,7 +2,7 @@ class AddFieldsToOrganization < ActiveRecord::Migration
   def change
     add_column :organizations, :alternate_name, :string
     add_column :organizations, :date_incorporated, :date
-    add_column :organizations, :description, :text, null: false
+    add_column :organizations, :description, :text, null: false, :default => 'No description found'
     add_column :organizations, :email, :string
     add_column :organizations, :legal_status, :string
     add_column :organizations, :tax_id, :string
--- db/migrate/20141009185459_add_fields_to_address.rb
+++ db/migrate/20141009185459_add_fields_to_address.rb
@@ -1,6 +1,6 @@
 class AddFieldsToAddress < ActiveRecord::Migration
   def change
-    add_column :addresses, :country_code, :string, null: false
+    add_column :addresses, :country_code, :string, null: false, :default => 'GB'
     add_column :addresses, :street_2, :string
     rename_column :addresses, :postcode, :postal_code
     rename_column :addresses, :street, :street_1
