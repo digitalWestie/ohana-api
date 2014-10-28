5e8bf85ec48f7694ed810d9531dbd913f71b3dec
Rory Gianni <giannichan@gmail.com>
2014-10-28 23:23:36 +0000

    adds new fields to location's full address

--- app/models/location.rb
+++ app/models/location.rb
@@ -113,7 +113,7 @@ class Location < ActiveRecord::Base
 
   def full_physical_address
     return unless address.present?
-    "#{address.street}, #{address.city}, #{address.state} #{address.postal_code}"
+    "#{address.street_1}, #{address.street_2}, #{address.city}, #{address.postal_code}"
   end
 
   def coordinates
