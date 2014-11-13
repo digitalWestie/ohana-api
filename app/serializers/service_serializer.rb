887508193531b0c27dd25c648e3214a3750c034c
Rory Gianni <giannichan@gmail.com>
2014-11-13 21:53:26 +0000

    adds schedules and locations as per availabilties to serializer

--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -2,11 +2,30 @@ class ServiceSerializer < ActiveModel::Serializer
   attributes :id, :accepted_payments, :alternate_name, :audience, :description,
              :eligibility, :email, :fees, :funding_sources, :how_to_apply,
              :keywords, :languages, :name, :required_documents, :service_areas,
-             :status, :website, :wait, :updated_at
+             :min_age, :max_age, :status, :website, :wait, :updated_at,
+             :availabilities
 
-  # embed :ids, include: true
   has_many :categories
-  has_many :availabilities
-  has_many :regular_schedules
-  has_many :holiday_schedules
+
+  def availabilities
+    result = []
+    for availability in object.availabilities
+
+      regular_schedules = object.regular_schedules.where(availability: availability).map do |rs|
+        RegularScheduleSerializer.new(rs).serializable_hash
+      end
+
+      holiday_schedules = object.holiday_schedules.where(availability: availability).map do |hs|
+        HolidayScheduleSerializer.new(hs).serializable_hash
+      end
+
+      result << {
+        location: availability.location,
+        regular_schedules: regular_schedules,
+        holiday_schedules: holiday_schedules
+      }
+    end
+    result
+  end
+
 end
