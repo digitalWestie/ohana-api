feaa0a30f65ea4b6f9b45349543439d837b1cb99
Rory Gianni <giannichan@gmail.com>
2014-11-13 16:17:05 +0000

    use admin layout for home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   # If the user is signed in, we'd like to greet
   # them on the Home page
+  layout 'admin'
+
   def index
     @user = current_user
   end
