915576c19765784797bb1a5b1560a34f99657387
Rory Gianni <giannichan@gmail.com>
2014-11-26 17:46:44 +0000

    expand on search methods

--- app/models/concerns/service_search.rb
+++ app/models/concerns/service_search.rb
@@ -1,7 +1,8 @@
 module ServiceSearch
   extend ActiveSupport::Concern
   included do
-    #scope :keyword, ->(keyword) { keyword_search(keyword) }
+    scope :keyword, ->(keyword) { keyword_search(keyword) }
+
     include PgSearch
     pg_search_scope :keyword_search,
       against: :search_vector,
@@ -13,5 +14,27 @@ module ServiceSearch
           tsvector_column: 'search_vector'
       }
     }
+  end
+
+  module ClassMethods
+    require 'exceptions'
+
+    def search(params = {})
+      text_search(params).uniq
+    end
+
+    def text_search(params = {})
+      allowed_params(params).reduce(self) do |relation, (scope_name, value)|
+        value.present? ? relation.public_send(scope_name, value) : relation.all
+      end
+    end
+
+    def status(param)
+      where(status: param)
+    end
+
+    def allowed_params(params)
+      params.slice(:keyword, :status)
+    end
   end
 end
