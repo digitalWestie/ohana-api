6d083ab36ba4394b2a7194577a941229224bbf3c
Rory Gianni <giannichan@gmail.com>
2014-11-26 15:56:42 +0000

    make sure availability has a service id

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -3,6 +3,7 @@ class Availability < ActiveRecord::Base
   belongs_to :service
   belongs_to :location
 
+  validates :service_id , presence: true
   validates :location_id, uniqueness: {scope: :service_id}, presence: true
 
   has_many :regular_schedules, dependent: :destroy
@@ -12,4 +13,7 @@ class Availability < ActiveRecord::Base
   has_many :holiday_schedules, dependent: :destroy
   accepts_nested_attributes_for :holiday_schedules,
                                 allow_destroy: true, reject_if: :all_blank
+
+  after_create :update_location_status
+
 end
