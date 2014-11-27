008b67a8bb98426a1f41dda6d381e379f3e700f0
Rory Gianni <giannichan@gmail.com>
2014-11-27 16:13:29 +0000

    formats operating hours of regular schedlues in api to HH:MM

--- app/serializers/regular_schedule_serializer.rb
+++ app/serializers/regular_schedule_serializer.rb
@@ -1,6 +1,14 @@
 class RegularScheduleSerializer < ActiveModel::Serializer
   attributes :weekday, :opens_at, :closes_at
 
+  def opens_at
+    object.opens_at.strftime('%H:%M')
+  end
+
+  def closes_at
+    object.closes_at.strftime('%H:%M')
+  end
+
   def weekday
     Admin::FormHelper::WEEKDAYS[object.weekday - 1]
   end
