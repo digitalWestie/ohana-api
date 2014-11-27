b42b4d3eb0b74fb15785fba35ee343a7b59d46c1
Rory Gianni <giannichan@gmail.com>
2014-11-27 16:21:42 +0000

    simplifies output formats of times for holiday schedules in api

--- app/serializers/holiday_schedule_serializer.rb
+++ app/serializers/holiday_schedule_serializer.rb
@@ -1,3 +1,19 @@
 class HolidayScheduleSerializer < ActiveModel::Serializer
   attributes :closed, :start_date, :end_date, :opens_at, :closes_at
+
+  def opens_at
+    object.opens_at.strftime('%H:%M') if object.opens_at
+  end
+
+  def closes_at
+    object.closes_at.strftime('%H:%M') if object.closes_at
+  end
+
+  def start_date
+    object.start_date.strftime('%F')
+  end
+
+  def end_date
+    object.end_date.strftime('%F')
+  end
 end
