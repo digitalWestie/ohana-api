9e9af20845d76ed903b7a5dbf364afcdd2e8c98e
Rory Gianni <giannichan@gmail.com>
2014-11-27 23:45:43 +0000

    remove unnecessary serializer

--- app/serializers/basic_service_serializer.rb
+++
@@ -1,8 +0,0 @@
-class BasicServiceSerializer < ActiveModel::Serializer
-  attributes :id, :accepted_payments, :alternate_name, :audience, :description,
-             :eligibility, :email, :fees, :funding_sources, :how_to_apply,
-             :keywords, :languages, :name, :required_documents, :service_areas,
-             :min_age, :max_age, :status, :website, :wait, :updated_at
-
-  has_many :categories
-end
