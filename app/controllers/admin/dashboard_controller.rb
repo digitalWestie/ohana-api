2e3e9863682a032cbbe8571bb1a9c9f70315f953
Rory Gianni <giannichan@gmail.com>
2014-12-12 22:15:56 +0000

    fixes log out error

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,7 +5,7 @@ class Admin
     def index
       redirect_to new_session_path(:admin) unless admin_signed_in?
       @admin = current_admin
-      @unapproved_orgs = Organization.unapproved.count if @admin.super_admin
+      @unapproved_orgs = Organization.unapproved.count if admin_signed_in? and @admin.super_admin
     end
   end
 end
