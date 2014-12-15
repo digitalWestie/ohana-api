263dfd1e84d74ec626ab653cca0345c4ede9f69a
Rory Gianni <giannichan@gmail.com>
2014-12-15 22:15:54 +0000

    adds a notification mailer for a new organisation

---
+++ app/mailers/notifications_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationsMailer < ActionMailer::Base
+  default from: SETTINGS[:admin_support_email]
+
+  def new_org_created(organization)
+    @organization = organization
+    admins = Admin.where(super_admin: true).pluck(:email)
+    mail :to => SETTINGS[:admin_support_email], :cc => admins, :subject => "New organisation on Clacks-Ohana"
+  end
+
+end
