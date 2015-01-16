901623bfb7afa94d678c3512898ed03f58c8d044
digitalWestie <giannichan@gmail.com>
2015-01-16 16:28:03 +0000

    adds counts of entities into  into links

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,7 +5,12 @@ class Admin
     def index
       redirect_to new_session_path(:admin) unless admin_signed_in?
       @admin = current_admin
+      @ad = ClacksAdminDecorator.new(current_admin)
       @unapproved_orgs = Organization.unapproved.count if admin_signed_in? and @admin.super_admin
+      @org_count = @ad.orgs_relation.count
+      @location_count = @ad.locations.count
+      @service_count = @ad.services.count
+      @program_count = @ad.programs.count
     end
   end
 end
